81e1ce5ddd4eff57764ab32133b28d0c8d1b9e44
ed4st <61949882+ed4st@users.noreply.github.com>
2020-09-08 22:47:50 -0500

    first version of reduce_to_echelon for simple matrices. It rests to take more complex matrices and cosider more cases

--- reduce_to_echelon.R
+++ reduce_to_echelon.R
@@ -28,16 +28,30 @@ elementary_3 = function(m, i, j, b){
   return(m)
 }
 
+
+first_nonzero_index = function(row){
+  return(min(which(row != 0)))
+}
 #The following function converts the matrix to a row echelon form
 
 reduce_to_echelon = function(m){
+  lead = 1
   nr = dim(m)[1]
   nc = dim(m)[2]
-  for(r in 1:nr){
-    #verifying if a row is 0
-    if(is_zero(m[r,])){
-      
+  
+  while( lead < nr){
+    for (r in (lead + 1):nr) {
+       a = -m[r,first_nonzero_index(m[r,])]/m[lead,first_nonzero_index(m[lead,])]
+       m = elementary_3(m, r, lead,a)
     }
-    
+    lead = lead + 1
   }
+  return(m)
 }
+
+
+m = matrix(c(1,2,3,4,5,6,7,8,9), nrow = 3, byrow = T)
+m
+m = elementary_3(m, 2, 1 , -4)
+elementary_3(m, 3, 1 , -7)
+reduce_to_echelon(m)
