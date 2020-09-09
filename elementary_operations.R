e037bf7974dec7e71ccfbc1ca0f47e480955986d
ed4st <61949882+ed4st@users.noreply.github.com>
2020-09-08 19:36:06 -0500

    echelon_form function added. It returns if the given matrix is in Echelon form

--- elementary_operations.R
+++ elementary_operations.R
@@ -29,7 +29,7 @@ loadMatrix = function(){
   return(mat)
 }
 
-#following matrix computes the type 1 elemental row operation
+#following function computes the type 1 elemental row operation
 elemental_1 = function(m, i, j){
   aux = m[i,]
   m[i,] = m[j,]
@@ -37,20 +37,88 @@ elemental_1 = function(m, i, j){
   return(m)
 }
 
-#following matrix computes the type 2 elemental row operation
+#following function computes the type 2 elemental row operation
 elemental_2 = function(m, i, a){
   m[i,] = a*m[i,] 
   return(m)
 }
 
-#following matrix computes the type 3 elemental row operation
+#following function computes the type 3 elemental row operation
 elemental_3 = function(m, i, j, b){
   m[i,] = m[i,] + b*m[j,]
   return(m)
 }
 
+#following function returns the question: 
+#Is the matrix m in Echelon form
+echelon_form = function(mat){
+  
+  #number of rows
+  m = dim(mat)[1]
+  #number of columns
+  n = dim(mat)[2]
+  #following arrays save the (i,j) position of first nonzero entry
+  #in backward column
+  pivots_i = c()
+  pivots_j = c()
+  for (j in 1:n){
+    if(!is_zero(mat[,j])){
+      pivots_j = c(pivots_j, j)
+      pivots_i = c(pivots_i, rowpivot(mat[,j]))
+    }
+  }
+  #veryfing that row pivot positions are increasing
+  if(length(pivots_i) > 0){
+    for (index in 1:max(pivots_i)) {
+      if(!(index %in% pivots_i[1:match(max(pivots_i),pivots_i)])){
+        return(FALSE)
+      }
+    }
+    return(!is.unsorted(pivots_i[1:match(max(pivots_i),pivots_i)]))
+  }
+  if(is.null(pivots_i)){
+    return(TRUE)
+  }
+  return(FALSE)
+}
+m = matrix(c(0,0,0,0,0,0,0,0,0,0,0,0), nrow = 3, byrow = T)
+m
+echelon_form(m)
 
-m = matrix(c(1,2,3,4,5,6,7,8,9), nrow = 3, byrow = T)
+is_almost_increasing = function(pivots_i){
+  
+}
+
+#following function verifies if a vector is the zero vector
+is_zero = function(c){
+  for(num in c){
+    if(num != 0){
+      return(FALSE)
+    }
+  }
+  return(TRUE)
+}
+
+
+#following function returns recursively the index
+#where the pivot is located in a nonzero column
+#cl is a column
+rowpivot = function (cl){
+  len = length(cl)
+  if(cl[len] != 0 | len == 0){
+    return(len)
+  }
+  else{
+    return(rowpivot(cl[1:(len-1)]))
+  }
+}
+
+
+m = matrix(c(1,2,3,4,0,6,0,8,0,0,0,0), nrow = 3, byrow = T)
+m
+echelon_form(m)
+
+dim(m)[1]
 elemental_3(m,3,1,-7)
 
 
